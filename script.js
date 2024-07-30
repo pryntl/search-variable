28c96d671d6502aa0c4193679429c10042c98aad
pryntl <pryntl2007@gmail.com>
2024-07-31 00:50:34 +0330

    feat [java script]
    added comments to explain the codes.

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+// main variable that we wanna search in.
 let string = ["sponge", "bob", "square", "pants"],
-  target1 = "square",
+// the targeted words.
+target1 = "square",
   target2 = "yellow";
+  //printing the variables.
 console.log("string:", string);
 console.log("target1:", target1);
 console.log("is target1 in the string?");
+// using "if" to see if the word exist in the main variable.
 if (target1 == string[0]) {
   console.log(true);
 } else if (target1 == string[1]) {
@@ -15,8 +19,10 @@ if (target1 == string[0]) {
 } else {
   console.log(false);
 }
+//printing the variables.
 console.log("target2:", target2);
 console.log("is target2 in the string?");
+// using "if" to see if the word exist in the main variable.
 if (target2 == string[0]) {
   console.log(true);
 } else if (target2 == string[1]) {
