2f7c8e5ebf757a230a8e928867f6e68253fc9bf3
pryntl <pryntl2007@gmail.com>
2024-07-31 00:45:27 +0330

    build [java script]

---
+++ script.js
@@ -0,0 +1,30 @@
+let string = ["sponge", "bob", "square", "pants"],
+  target1 = "square",
+  target2 = "yellow";
+console.log("string:", string);
+console.log("target1:", target1);
+console.log("is target1 in the string?");
+if (target1 == string[0]) {
+  console.log(true);
+} else if (target1 == string[1]) {
+  console.log(true);
+} else if (target1 == string[2]) {
+  console.log(true);
+} else if (target1 == string[3]) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+console.log("target2:", target2);
+console.log("is target2 in the string?");
+if (target2 == string[0]) {
+  console.log(true);
+} else if (target2 == string[1]) {
+  console.log(true);
+} else if (target2 == string[2]) {
+  console.log(true);
+} else if (target2 == string[3]) {
+  console.log(true);
+} else {
+  console.log(false);
+}
