ffc04b8f2b738b07ca56ecb644c34fc21302de6a
pryntl <pryntl2007@gmail.com>
2024-07-31 15:45:58 +0330

    fix [java script]
    fixed a problem in code by turning the variable from an array into a string.

--- script.js
+++ script.js
@@ -1,35 +1,22 @@
 // main variable that we wanna search in.
-let string = ["sponge", "bob", "square", "pants"],
+let string = "sponge bob square pants.";
 // the targeted words.
-target1 = "square",
-  target2 = "yellow";
-  //printing the variables.
+(target1 = "square"), (target2 = "yellow");
+//printing the variables.
 console.log("string:", string);
 console.log("target1:", target1);
 console.log("is target1 in the string?");
 // using "if" to see if the word exist in the main variable.
-if (target1 == string[0]) {
-  console.log(true);
-} else if (target1 == string[1]) {
-  console.log(true);
-} else if (target1 == string[2]) {
-  console.log(true);
-} else if (target1 == string[3]) {
+if (string.includes(target1)) {
   console.log(true);
 } else {
   console.log(false);
 }
-//printing the variables.
+//printing the variable.
 console.log("target2:", target2);
 console.log("is target2 in the string?");
 // using "if" to see if the word exist in the main variable.
-if (target2 == string[0]) {
-  console.log(true);
-} else if (target2 == string[1]) {
-  console.log(true);
-} else if (target2 == string[2]) {
-  console.log(true);
-} else if (target2 == string[3]) {
+if (string.includes(target2)) {
   console.log(true);
 } else {
   console.log(false);
